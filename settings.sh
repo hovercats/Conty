7db439c819b9c9ec3fbec6d03db037e49ecbd6a7
hovercats <hovercatswithlasereyes@protonmail.com>
2026-02-24 22:52:14 +0100

    remove unneccessary locale

--- settings.sh
+++ settings.sh
@@ -53,11 +53,11 @@ PACKAGES=(
 	nano ttf-dejavu ttf-liberation firefox mpv geany pcmanfm
 	htop qbittorrent speedcrunch gpicview file-roller openbox lxterminal
 	yt-dlp minizip nautilus genymotion jre17-openjdk gnome-themes-extra
- 	ffmpegthumbnailer tmux jdownloader2 soundconverter mame-tools
+ 	ffmpegthumbnailer tmux soundconverter mame-tools
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(cemu)
+AUR_PACKAGES=(cemu jdownloader2)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
@@ -76,31 +76,7 @@ ALHP_FEATURE_LEVEL=2
 
 # Locales to configure in the image
 LOCALES=(
-	'ar_EG.UTF-8 UTF-8'
 	'en_US.UTF-8 UTF-8'
-	'en_GB.UTF-8 UTF-8'
-	'en_CA.UTF-8 UTF-8'
-	'en_SG.UTF-8 UTF-8'
-	'es_MX.UTF-8 UTF-8'
-	'zh_CN.UTF-8 UTF-8'
-	'fr_FR.UTF-8 UTF-8'
-	'ru_RU.UTF-8 UTF-8'
-	'ru_UA.UTF-8 UTF-8'
-	'es_ES.UTF-8 UTF-8'
-	'de_DE.UTF-8 UTF-8'
-	'pt_BR.UTF-8 UTF-8'
-	'it_IT.UTF-8 UTF-8'
-	'id_ID.UTF-8 UTF-8'
-	'ja_JP.UTF-8 UTF-8'
-	'bg_BG.UTF-8 UTF-8'
-	'pl_PL.UTF-8 UTF-8'
-	'da_DK.UTF-8 UTF-8'
-	'ko_KR.UTF-8 UTF-8'
-	'tr_TR.UTF-8 UTF-8'
-	'hu_HU.UTF-8 UTF-8'
-	'cs_CZ.UTF-8 UTF-8'
-	'bn_IN UTF-8'
-	'hi_IN UTF-8'
 )
 
 # Content of pacman mirrorrlist file before reflector is installed and used to fetch new one
